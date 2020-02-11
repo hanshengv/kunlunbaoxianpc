63b62de8f1b6d9c5dc0a84fa1f4267489053d2f8
qq943153000 <943153000@qq.com>
2020-02-11 12:23:13 +0800

    第三次没思路

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,38 @@
 <template>
-  <div class="home">
-    <h1>home page</h1>
-  </div>
+    <div class="home">
+        <div class='top'>
+            <div class='logo'></div>
+            <div class='toplist'>
+                <li>登录</li>
+                <li>个人中心</li>
+                <li>企业中心</li>
+                <li>公开信息披露</li>
+            </div>
+        </div>
+        <div class='center'>
+            <ul>
+                <li>首页</li>
+                <li>在线投保</li>
+                <li>保单服务</li>
+                <li>健康服务</li>
+            </ul>
+        </div>
+        <div class='nav'></div>
+    </div>
 </template>
+<style lang='scss'>
+    .home  {
+        width: 100%;
+        height: 100%;
+        .top {
+            display: flex;
+            .logo {
+                flex: 1;
+            }
+
+        }
+    }
+</style>
 
 <script>
 
